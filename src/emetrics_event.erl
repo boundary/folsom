d759a551233ba88103965743f97a27cb9ff4e1aa
joewilliams <joe@joetify.com>
2011-03-22 19:12:14 -0700

    fix up stat functions

--- src/emetrics_event.erl
+++ src/emetrics_event.erl
@@ -15,7 +15,16 @@
          delete_handler/1,
          notify/1,
          get_values/1,
-         get_info/1]).
+         get_info/1,
+         get_max/1,
+         get_min/1,
+         get_histogram/1,
+         get_histogram/2,
+         get_variance/1,
+         get_mean/1,
+         get_median/1,
+         get_percentile/2,
+         get_all/1]).
 
 %% gen_event callbacks
 -export([init/1, handle_event/2, handle_call/2,
@@ -27,17 +36,12 @@
           sample
          }).
 
+-define(HIST, [10, 20, 30, 50, 100, 200, 300, 400, 500, 1000, 99999999999999]).
+
 %%%===================================================================
 %%% gen_event callbacks
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Adds an event handler
-%%
-%% @spec add_handler() -> ok | {'EXIT', Reason} | term()
-%% @end
-%%--------------------------------------------------------------------
 add_handler(Id, Type, Size) ->
     gen_event:add_sup_handler(emetrics_event_manager,
                               {emetrics_event, Id}, [Id, Type, Size]).
@@ -57,6 +61,77 @@ get_values(Id) ->
 
 get_info(Id) ->
     gen_event:call(emetrics_event_manager, {emetrics_event, Id}, info).
+
+get_max(Id) when is_atom(Id) ->
+    get_max(get_values(Id));
+get_max([]) ->
+    0;
+get_max(Values) ->
+    [Head | _] = lists:reverse(lists:sort(Values)),
+    Head.
+
+get_min(Id) when is_atom(Id)->
+    get_min(get_values(Id));
+get_min([]) ->
+    0;
+get_min(Values) ->
+    [Head | _] = lists:sort(Values),
+    Head.
+
+get_histogram(Id) ->
+    get_histogram(Id, ?HIST).
+
+get_histogram(Id, Hist) ->
+    Bins = [{Bin, 0} || Bin <- Hist],
+    Values = get_values(Id),
+    build_hist(Values, Bins).
+
+get_variance(Id) ->
+    get_variance(Id, get_values(Id)).
+
+get_variance(_, []) ->
+    0;
+get_variance(Id, Values) ->
+    Mean = get_mean(Id),
+    variance(Mean, Values, 0) / length(Values).
+
+get_mean(Id) when is_atom(Id) ->
+    get_mean(get_values(Id));
+get_mean([]) ->
+    0;
+get_mean(Values) ->
+    Sum = lists:sum(Values),
+    Sum / length(Values).
+
+get_median(Id) ->
+    get_percentile(Id, 0.5).
+
+get_percentile(Id, Percentile) when is_atom(Id) ->
+    get_percentile(lists:sort(get_values(Id)), Percentile);
+get_percentile([], _) ->
+    0;
+get_percentile(Values, Percentile) ->
+    Element = round(Percentile * length(Values)),
+    lists:nth(Element, Values).
+
+get_all(Id) ->
+    {Id, Type} = get_info(Id),
+    [
+     {id, Id},
+     {type, Type},
+     {min, get_min(Id)},
+     {max, get_max(Id)},
+     {mean, get_mean(Id)},
+     {median, get_median(Id)},
+     {variance, get_variance(Id)},
+     {percentile,
+      [
+      {75, get_percentile(Id, 0.75)},
+      {99, get_percentile(Id, 0.99)}
+      ]
+     },
+     {histogram, get_histogram(Id)}
+     ].
 
 %%--------------------------------------------------------------------
 %% @private
@@ -113,13 +188,13 @@ handle_event(_, State) ->
 %% @end
 %%--------------------------------------------------------------------
 handle_call(info, #metric{id = Id, type = Type} = State) ->
-    {ok, [{id, Id}, {type, Type}], State};
-handle_call(values, #metric{id = Id, type = uniform = Type, sample = Sample} = State) ->
+    {ok, {Id, Type}, State};
+handle_call(values, #metric{type = uniform, sample = Sample} = State) ->
     Values = emetrics_uniform:get_values(Sample),
-    {ok, [{id, Id}, {type, Type}, {sample, Values}], State};
-handle_call(values, #metric{id = Id, type = exdec = Type, sample = Sample} = State) ->
+    {ok, Values, State};
+handle_call(values, #metric{type = exdec, sample = Sample} = State) ->
     Values = emetrics_exdec:get_values(Sample),
-    {ok, [{id, Id}, {type, Type}, {sample, Values}], State}.
+    {ok, Values, State}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -164,3 +239,25 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+variance(Mean, [Head | Tail], Acc) ->
+    V = Acc + math:pow(Head - Mean, 2),
+    variance(Mean, Tail, V);
+variance(_, [], Acc) ->
+    Acc.
+
+% all this is too complicated, find better solution
+build_hist([Head | Tail], Hist) ->
+    {Bin, Count} = proplists:lookup(which_bin(Head, Hist, []), Hist),
+    List = proplists:delete(Bin, Hist),
+    NewHist = lists:append(List, [{Bin, Count + 1}]),
+    build_hist(Tail, NewHist);
+build_hist([], Hist) ->
+    lists:sort(Hist).
+
+which_bin(Value, [{Bin, _} = B | Tail], Acc) when Value =< Bin ->
+    which_bin(Value, Tail, lists:sort(lists:append(Acc, [B])));
+which_bin(Value, [_ | Tail], Acc) ->
+    which_bin(Value, Tail, Acc);
+which_bin(_, [], [{Bin, _} | _]) ->
+    Bin.
