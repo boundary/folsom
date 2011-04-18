46837244cea9d4059c6db6d099bf39433730142f
joewilliams <joe@joetify.com>
2011-04-18 13:58:31 -0700

    add counters and gauges

---
+++ src/folsom_metrics_counter.erl
@@ -0,0 +1,56 @@
+%%%
+%%% Copyright 2011, Boundary
+%%%
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%
+
+
+%%%-------------------------------------------------------------------
+%%% File:      folsom_metrics_counter.erl
+%%% @author    joe williams <j@boundary.com>
+%%% @copyright 2011 Boundary
+%%% @doc
+%%% @end
+%%%-----------------------------------------------------------------
+
+-module(folsom_metrics_counter).
+
+-export([new/1,
+         inc/1,
+         inc/2,
+         dec/1,
+         dec/2,
+         get/1,
+         clear/1]).
+
+new(Name) ->
+    Counter = {Name, 0},
+    ets:insert(?COUNTER_TABLE, Counter).
+
+inc(Name) ->
+    ets:update_counter(?COUNTER_TABLE, Name, 1).
+
+inc(Name, Value) ->
+    ets:update_counter(?COUNTER_TABLE, Name, Value).
+
+dec(Name) ->
+    ets:update_counter(?COUNTER_TABLE, Name, -1).
+
+dec(Name, Value) ->
+    ets:update_counter(?COUNTER_TABLE, Name, Value * -1).
+
+get(Name) ->
+    ets:lookup(?COUNTER_TABLE, Name).
+
+clear(Name) ->
+    ets:insert(?COUNTER_TABLE, {Name, 0}),
---
+++ src/folsom_metrics_gauge.erl
@@ -0,0 +1,41 @@
+%%%
+%%% Copyright 2011, Boundary
+%%%
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%
+
+
+%%%-------------------------------------------------------------------
+%%% File:      folsom_metrics_gauge.erl
+%%% @author    joe williams <j@boundary.com>
+%%% @copyright 2011 Boundary
+%%% @doc
+%%% @end
+%%%-----------------------------------------------------------------
+
+-module(folsom_metrics_gauge).
+
+-export([new/1,
+         update/2,
+         clear/1]).
+
+new(Name) ->
+    Gauge = {Name, 0},
+    ets:insert(?GAUGE_TABLE, Gauge).
+
+update(Name, Value) ->
+    Gauge = {Name, Value},
+    ets:insert(?GAUGE_TABLE, Gauge).
+
+clear(Name) ->
+    new(Name).
