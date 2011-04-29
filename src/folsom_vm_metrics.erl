507066d0bf1aa0f77a4e0ee621361c93f43bf9e0
amtal <alex.kropivny@gmail.com>
2011-04-29 11:18:01 -0700

    atom instead of string keys on stats

--- src/folsom_vm_metrics.erl
+++ src/folsom_vm_metrics.erl
@@ -50,20 +50,22 @@ convert_statistics(context_switches, {ContextSwitches, 0}) ->
     ContextSwitches;
 convert_statistics(exact_reductions, {TotalExactReductions,
                                         ExactReductionsSinceLastCall}) ->
-    [{"total_exact_reductions", TotalExactReductions},
-     {"exact_reductions_since_last_call", ExactReductionsSinceLastCall}];
+    [{total_exact_reductions, TotalExactReductions},
+     {exact_reductions_since_last_call, ExactReductionsSinceLastCall}];
 convert_statistics(garbage_collection, {NumberofGCs, WordsReclaimed, 0}) ->
-    [{"number_of_gcs", NumberofGCs}, {"words_reclaimed", WordsReclaimed}];
+    [{number_of_gcs, NumberofGCs},
+     {words_reclaimed, WordsReclaimed}];
 convert_statistics(io, {Input, Output}) ->
     [Input, Output];
 convert_statistics(reductions, {TotalReductions, ReductionsSinceLastCall}) ->
-    [{"total_reductions", TotalReductions},
-     {"reductions_since_last_call", ReductionsSinceLastCall}];
+    [{total_reductions, TotalReductions},
+     {reductions_since_last_call, ReductionsSinceLastCall}];
 convert_statistics(runtime, {TotalRunTime, TimeSinceLastCall}) ->
-    [{"total_run_time", TotalRunTime}, {"time_since_last_call", TimeSinceLastCall}];
+    [{total_run_time, TotalRunTime},
+     {time_since_last_call, TimeSinceLastCall}];
 convert_statistics(wall_clock, {TotalWallclockTime, WallclockTimeSinceLastCall}) ->
-    [{"total_wall_clock_time", TotalWallclockTime},
-     {"wall_clock_time_since_last_call", WallclockTimeSinceLastCall}];
+    [{total_wall_clock_time, TotalWallclockTime},
+     {wall_clock_time_since_last_call, WallclockTimeSinceLastCall}];
 convert_statistics(_, Value) ->
     Value.
 
